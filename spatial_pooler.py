c886a71f218fa2d31d9440dbb906c7119ef7866e
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-04-28 20:21:06 +0200

    Added basic structure of spatial_pooler.py

---
+++ spatial_pooler.py
@@ -0,0 +1,12 @@
+import numpy as np
+import sys, random, json
+
+def activation(state):
+
+def permanence(state):
+
+def potential_pool(state):
+
+def spatial_pooler(state):
+    #State: Input SDR as np.array (bool)
+    return activation(permanence(potential_pool(state)))
