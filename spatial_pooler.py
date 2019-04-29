ac997292fda5233d01e265d66a8af331a0fca709
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-04-29 17:37:59 +0200

    Added defaults to __init__ function and made treshhold_activation into a percentage

--- spatial_pooler.py
+++ spatial_pooler.py
@@ -1,4 +1,4 @@
-
+#! /usr/bin/python3
 import numpy as np
 import sys, random, json
 
@@ -7,33 +7,34 @@ import sys, random, json
 class SpatialPooler():
 
 
-    def __init__(self,num_collumns,threshhold_permances,threshhold_activation,size_of_potential_pool):
+    def __init__(self,num_collumns,threshhold_permances=0.5,threshhold_activation=0.6,size_of_potential_pool=0.75):
         self.current_collumn = 0
-        self.thershhold_permanences = threshhold_permanences
+        self.thershhold_permanences = threshhold_permances
         self.threshhold_activation = threshhold_activation
         self.collumns = self.init_collumn(num_collumns,1-size_of_potential_pool)
 
-    def init_collumn(self,num_collumns,size_of_potential_pool):
+    def init_collumn(self,num_collumns,size_of_potential_pool=0.75):
         collumns = [{} for i in range(num_collumns)]
         for i in range(num_collumns):
-            collumns[i].permanences = np.random.rand(num_collumns) # Random values for the permanence between  0-1
-            collumns[i].potential_pool = np.random.rand(num_collumns) > size_of_potential_pool # Create a random potential pool with a certain percantage of potential connections
+            collumns[i]['permanences'] = np.random.rand(num_collumns) # Random values for the permanence between  0-1
+            collumns[i]['potential_pool'] = np.random.rand(num_collumns) > size_of_potential_pool # Create a random potential pool with a certain percantage of potential connections
         return collumns
 
     def activation(self,state):
         """
         Sum state and then compare that to threshhold_activation
         """
-        return np.sum(state) > self.threshold_activation
+        (length,) = state.shape
+        return np.sum(state)/length > self.threshold_activation
 
     def permanence(self,state):
         """
         For all permanences,filter the state for those that are higher than  self.theta
         """
-        return (self.collumns[self.current_collumn].permanences > self.thershhold_permanence) * state
+        return (self.collumns[self.current_collumn]['permanences'] > self.thershhold_permanence) * state
 
     def potential_pool(self,state):
-        return self.collumns[self.current_collumn].potential_pool * state
+        return self.collumns[self.current_collumn]['potential_pool'] * state
 
     def spatial_pooler(self,state):
         """
