d947d71b0ac0e65bf524d9787d7cff6f79e1f2b3
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-04-29 12:16:26 +0200

    permanence function added

--- spatial_pooler.py
+++ spatial_pooler.py
@@ -1,16 +1,28 @@
 import numpy as np
 import sys, random, json
 
-def activation(state):
 
-def permanence(state):
 
-def potential_pool(state):
+class SpatialPooler():
 
-def spatial_pooler(state,sp):
-    """
-    state := np.array (bool)
-    Spatial Pooler:
-    Input -> potetial pool -> permanence -> activation -> output
-    """
-    return activation(permanence(potential_pool(state)))
+
+    def __init__(self,theta):
+        self.current_collumn = 0
+        self.theta = theta
+        
+        
+    def activation(self,state):
+        
+    def permanence(self,state):
+        return (self.collumns[self.current_collumn].permanences > self.theta) * state
+
+    def potential_pool(self,state):
+        return self.collumns[self.current_collumn].potential_pool * state
+
+    def spatial_pooler(self,state):
+        """
+        state := np.array (bool)
+        Spatial Pooler:
+        Input -> potetial pool -> permanence -> activation -> output
+        """
+        return self.activation(self.permanence(self.potential_pool(state)))
